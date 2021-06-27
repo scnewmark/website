665c1a695498d19e9e0c25f930dcb27f58375ca3
Sap ★ <samuel.newmark10@gmail.com>
2021-06-27 13:45:26 -0500

    feat(client): add language attr add update SEO

--- client/components/seo/index.tsx
+++ client/components/seo/index.tsx
@@ -8,6 +8,9 @@ const SEO = (props: SEOProps) =>
 		<Head>
 			<title>{props.name}</title>
 			<link rel="icon" href="/images/favicon.png"/>
+			<meta name="description" content="Sam Newmark's personal website."/>
+			<meta name="keywords" content="Programming, Blog, Coding"/>
+			<meta name="author" content="Sam Newmark"/>
 
 			<meta property="og:site_name" content="Sam Newmark"/>
 			{Object.keys(props.openGraph).map((key: string, idx: number) =>
